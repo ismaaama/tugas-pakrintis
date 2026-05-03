2ab041887df836836365a488addc302cf934cea0
ismaaaama <iismaisma783@gmail.com>
2026-05-03 20:35:17 +0700

    add: tugas baru

---
+++ latihan5_2.py
@@ -0,0 +1,9 @@
+# Program Hitung Rata-rata
+
+a = float(input("Masukkan nilai pertama: "))
+b = float(input("Masukkan nilai kedua: "))
+c = float(input("Masukkan nilai ketiga: "))
+
+rata_rata = (a + b + c) / 3
+
+print("Rata-rata =", rata_rata)
